a481a803a3176df30ed7e2c879696c3d90496a05
AlleyTerry <art8689@nyu.edu>
2024-01-28 12:29:06 -0500

    started unity project
    opened unity project and placed some objects in the scene

---
+++ Week1_AllisonTerry/Assets/Scripts/WASD.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD : MonoBehaviour
+{
+    public float forceAmt;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
